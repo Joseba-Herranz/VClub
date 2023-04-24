dcefe4b5cbb01d8f408fb46a76ee1f3c137332cc
Joseba Andoni Herranz <josdron2@gmail.com>
2023-04-24 13:19:56 +0200

    userpage

--- front/js/sMovies.js
+++ front/js/sMovies.js
@@ -103,6 +103,7 @@ function withScore(){
  //Show the list
  function showmyList(result){
     console.log(result);
+    var mov = "--";
     result.data.forEach(someID => {
         console.log(someID.movieID);
         movieID=someID.movieID;
@@ -110,9 +111,8 @@ function withScore(){
             method: 'GET',
             redirect: 'follow'
         };
-          var mov = "</br>";
-        //   var nulo = 0;
-        //   var no_nulo = 0;
+         mov = "</br>";
+
         fetch("http://127.0.0.1:8000/api/allMovies", requestOptions)
             .then(response => response.json())
             .then(result => {
@@ -120,17 +120,12 @@ function withScore(){
                     // console.log(element);
                     if(movieID==element.movieID){
                     mov += `<h4>${element.name}</h4> </br>`;
-                    // mov += `<p>Year: ${element.year} </p> </br>`;
                     if(element.rankscore != null){
                         // console.log("No es nulo " + element.rankscore);
-                        // no_nulo++;
-                        // console.log(no_nulo);
                         mov += `<p>Year: ${element.year} Score: ${element.rankscore}</p>`;
                         
                     }else{
                         // console.log("Es nulo " + element.rankscore);
-                        // nulo++;
-                        // console.log(nulo);
                         mov += `<p>Year: ${element.year} Score: No Score</p>`;
                     }
                     mov += `<button onclick="addList(${element.movieID})" >My List</button>`;
@@ -143,6 +138,5 @@ function withScore(){
             })
             .catch(error => console.log('error', error));
     });
-    
     
  }
