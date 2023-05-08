bfa8d6835a92ddf9f382567db091db64878a3f4a
Joseba Andoni Herranz <josdron2@gmail.com>
2023-05-08 13:18:42 +0200

    login changes

--- front/js/login.js
+++ front/js/login.js
@@ -1,68 +1,95 @@
-function login(){
-    const email = document.getElementById('logE').value;
-    const password = document.getElementById('logP').value;
-
-    var formdata = new FormData();
-    formdata.append("email", email);
-    formdata.append("password", password);
+function vistaPrev(){
+  document.getElementById("save").addEventListener("click", function(event) {
+        
+    event.preventDefault();
     
-    var requestOptions = {
+    guardado();
+});
+
+document.getElementById("regis").addEventListener("click", function(event) {
+    
+    event.preventDefault();
+    
+    registro();
+});
+
+document.getElementById("login").addEventListener("click", function(event) {
+    
+    event.preventDefault();
+    
+    login();
+});
+
+  /////////
+  //Login//
+  /////////
+
+  function login(){
+      const email = document.getElementById('logE').value;
+      const password = document.getElementById('logP').value;
+
+      var formdata = new FormData();
+      formdata.append("email", email);
+      formdata.append("password", password);
+      
+      var requestOptions = {
+        method: 'POST',
+        body: formdata,
+        redirect: 'follow'
+      };
+      
+      fetch("http://127.0.0.1:8000/api/login", requestOptions)
+        .then(response => response.json())
+        .then(result => {
+          localStorage.setItem("token", result['authorisation']['token']);
+          document.getElementById("form").style.display = "none";
+          document.getElementById("contenido").style.display = "block";
+        })
+        .catch(error => {
+          alert("Tu correo o contraseña no son correctos");
+        });
+  }
+
+  ////////////
+  //Registro//
+  ////////////
+
+  function registro(){
+      const nombre = document.getElementById('regN').value;
+      const email = document.getElementById('regE').value;
+      const password = document.getElementById('regP').value;
+
+
+      var formdata = new FormData();
+      formdata.append("name", nombre);
+      formdata.append("email", email);
+      formdata.append("password", password);
+
+      var requestOptions = {
       method: 'POST',
       body: formdata,
       redirect: 'follow'
-    };
-    
-    fetch("http://hz114496:1912/api/login", requestOptions)
+      };
+
+      fetch("http://127.0.0.1:8000/api/register", requestOptions)
       .then(response => response.json())
       .then(result => {
-        localStorage.setItem("token", result['authorisation']['token']);
-        document.getElementById("form").style.display = "none";
-        document.getElementById("contenido").style.display = "block";
+          localStorage.setItem("token", result['authorisation']['token']);
+          // localStorage.setItem("antes", ) Prueba para saber si el usuario estaba logeado antes 
+          document.getElementById("form").style.display = "none";
+          document.getElementById("contenido").style.display = "block";
       })
       .catch(error => {
-        alert("Tu correo o contraseña no son correctos");
+        alert("El correo que esta usando esta en uso o su contraseña es menor del minimo");
       });
-}
+  }
 
-////////////
-//Registro//
-////////////
+  ////////////
+  //Log Out///
+  ////////////
 
-function registro(){
+  function logOut(){
+      localStorage.removeItem("token");
+  }
 
-    const nombre = document.getElementById('regN').value;
-    const email = document.getElementById('regE').value;
-    const password = document.getElementById('regP').value;
-
-
-    var formdata = new FormData();
-    formdata.append("name", nombre);
-    formdata.append("email", email);
-    formdata.append("password", password);
-
-    var requestOptions = {
-    method: 'POST',
-    body: formdata,
-    redirect: 'follow'
-    };
-
-    fetch("http://hz114496:1912/api/register", requestOptions)
-    .then(response => response.json())
-    .then(result => {
-        localStorage.setItem("token", result['authorisation']['token']);
-        // localStorage.setItem("antes", ) Prueba para saber si el usuario estaba logeado antes 
-        document.getElementById("form").style.display = "none";
-        document.getElementById("contenido").style.display = "block";
-    })
-    .catch(error => {
-      alert("El correo que esta usando esta en uso o su contraseña es menor del minimo");
-    });
-}
-
-////////////
-//Log Out///
-////////////
-
-function logOut(){
-    localStorage.removeItem("token");
 }
--- front/js/sMovies.js
+++ front/js/sMovies.js
@@ -5,25 +5,15 @@ function allMovies(){
         redirect: 'follow'
     };
       var mov = "</br>";
-    //   var nulo = 0;
-    //   var no_nulo = 0;
+
     fetch("http://127.0.0.1:8000/api/allMovies", requestOptions)
         .then(response => response.json())
         .then(result => {
             result.data.forEach(element => {
-                // console.log(element);
                 mov += `<h4>${element.name}</h4> </br>`;
-                // mov += `<p>Year: ${element.year} </p> </br>`;
                 if(element.rankscore != null){
-                    // console.log("No es nulo " + element.rankscore);
-                    // no_nulo++;
-                    // console.log(no_nulo);
                     mov += `<p>Year: ${element.year} Score: ${element.rankscore}</p>`;
-                    
                 }else{
-                    // console.log("Es nulo " + element.rankscore);
-                    // nulo++;
-                    // console.log(nulo);
                     mov += `<p>Year: ${element.year} Score: No Score</p>`;
                 }
                 mov += `<button onclick="addList(${element.movieID})" >My List</button>`;
@@ -33,7 +23,6 @@ function allMovies(){
             });
         })
         .catch(error => console.log('error', error));
-
 }
 
 //Only movies with score
@@ -111,4 +100,3 @@ function withScore(){
     })
     .catch(error => console.log('error', error));
  }
-
